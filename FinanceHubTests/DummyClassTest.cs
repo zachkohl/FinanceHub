f6196be1eac7b8639d15931ca112d9a7175f3935
Zach <zach.kohl@gmail.com>
2024-12-07 16:12:37 -0800

    Add project files.

---
+++ FinanceHubTests/DummyClassTest.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+namespace FinanceHub.Tests
+{
+    public class DummyClass_testData_returns3 //smoke test for testing framework. Can be deleted after several more tests are developed. 
+    {
+        [Fact]
+        public void Test1()
+        {
+            int x = FinanceHub.Controllers.DummyClass.testData;
+            Assert.Equal(3,x);
+        }
+    }
+}
---
+++ FinanceHubTests/TabHolder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FinanceHub.Tests
+{
+    public class TabHolder_tests
+    {
+        [Theory]
+        [InlineData('input')]
+        [InlineData('Data')]
+
+        public void TabHolder_opens_in_the_correct_tab(string tab)
+        {
+
+        }
+    }
+ 
+}
