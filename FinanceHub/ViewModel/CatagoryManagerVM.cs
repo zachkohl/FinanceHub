becee6f22461f3c6b9acccf651a747ea345a7868
Zach <zach.kohl@gmail.com>
2025-01-08 14:55:19 -0800

    Catagory Manger UI is roughed out

--- FinanceHub/ViewModel/CatagoryManagerVM.cs
+++ FinanceHub/ViewModel/CatagoryManagerVM.cs
@@ -1,30 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using FinanceHub.Services;
+using FinanceHub.View;
+
+
 
 namespace FinanceHub.ViewModel
 {
     public class CatagoryManagerVM: ViewModelBase
     {
 
-		private ObservableCollection<string> catagories=null!;
+		private ObservableCollection<Catagory> catagories=null!;
 
-		public ObservableCollection<string> Catagories
+		public ObservableCollection<Catagory> Catagories
 		{
 			get { return catagories; }
 			set { catagories = value; OnPropertyChanged(); }
 		}
 
-		public CatagoryManagerVM(TransactionsService transactionsService)
-		{
-			var l = new List<string>(["gas", "groceries", "utilities"]);
+        public ICommand PickColorCommand { get; set; }
+        private void PickColor(object obj)
+        {
+       
+            ColorPicker colorWindow = new ColorPicker();
+            colorWindow.Show();
 
-            Catagories = new ObservableCollection<string>(l);
-		}
+        }
+
+
+
+        public CatagoryManagerVM()
+        {
+            var l = new List<Catagory> { new Catagory { name = "gas", color = "Blue" }, new Catagory { name = "groceries", color = "Red" }, new Catagory { name = "utilities", color = "Yellow" } };
+
+            Catagories = new ObservableCollection<Catagory>(l);
+            PickColorCommand = new RelayCommand(PickColor);
+        }
+
+        public CatagoryManagerVM(TransactionsService transactionsService)
+        {
+            var l = new List<Catagory> { new Catagory { name = "gas", color = "Blue" }, new Catagory { name = "groceries", color = "Red" }, new Catagory { name = "utilities", color = "Yellow" } };
+
+            Catagories = new ObservableCollection<Catagory>(l);
+            PickColorCommand = new RelayCommand(PickColor);
+        }
 
 	}
+
+  public  class Catagory
+    {
+        public string name { get; set; } = null!;
+        public string color { get; set; } = null!;
+    }
+
 }
